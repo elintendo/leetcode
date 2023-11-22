ecd06ded085fe9f814301b2f222cf27c1b953cf4
elintendo <elintendo@yandex.ru>
2023-11-22 18:01:58 +0300

    356_lineReflection

---
+++ go/356_lineReflection.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func isReflected(points [][]int) bool {
+	mp := map[[2]int]bool{}
+	inf := 1 << 30
+	x_min, x_max := inf, -inf
+
+	for _, v := range points {
+		x_min = min(x_min, v[0])
+		x_max = max(x_max, v[0])
+		mp[[2]int{v[0], v[1]}] = true
+	}
+
+	sum := x_min + x_max
+
+	for _, v := range points {
+		if !mp[[2]int{sum - v[0], v[1]}] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isReflected([][]int{{1, 3}, {-1, 2}}))
+}
